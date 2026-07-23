06f7d75fdb2c0d2dc2db84d18eda91f211b7f5f1
ooeceek <indirtmcemini@gmail.com>
2026-07-23 16:25:53 +0300

    fix(ui): add missing FilterChipDefaults import

--- android/app/src/main/java/com/secondbrain/ui/tasks/TaskEditScreen.kt
+++ android/app/src/main/java/com/secondbrain/ui/tasks/TaskEditScreen.kt
@@ -36,6 +36,7 @@ import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
